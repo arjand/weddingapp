03bf7b582671d00746980c0b7db625a6d22d6796
arjand <adehar@gmail.com>
2013-11-10 20:31:17 -0800

    Add basic routes to app.

--- app/router.js
+++ app/router.js
@@ -1,11 +1,12 @@
 var Router = Ember.Router.extend(); // ensure we don't share routes between all Router instances
 
 Router.map(function() {
-  this.route('component-test');
-  this.route('helper-test');
-  // this.resource('posts', function() {
-  //   this.route('new');
-  // });
+  this.route('login');
+  this.resource('admin', function() {
+    this.route('index', { path: '/' });
+    this.route('guests');
+  });
+
 });
 
 export default Router;
